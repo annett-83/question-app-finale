5dba8260ee728989698639461f9129212540e3ef
annett-83 <74960058+annett-83@users.noreply.github.com>
2023-08-11 17:23:52 +0300

    Update userTeacherCard.jsx

--- client/src/components/teacherPage/userTeacherCard.jsx
+++ client/src/components/teacherPage/userTeacherCard.jsx
@@ -8,11 +8,7 @@ const UserTeacherCard = ({ userTeacher }) => {
 
                 <div className="d-flex flex-column align-items-center text-center position-relative">
                     <img
-                        src={`https://avatars.dicebear.com/api/avataaars/${(
-                            Math.random() + 1
-                        )
-                            .toString(36)
-                            .substring(7)}.svg`}
+                        src={userTeacher.image}
                         className="rounded-circle shadow-1-strong me-3"
                         alt="avatar"
                         width="65"
